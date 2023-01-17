0f9eeeef8e327b2be0027fb1602de8be10d207de
jebo-petra <john.jebo@petraonline.com>
2023-01-17 16:06:24 +0000

    login page

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,22 @@
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+
+
+export default function Copyright(props: any) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="#">
+          RGT
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+  
